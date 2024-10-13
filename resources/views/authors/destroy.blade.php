39ff9f20bd0a847272b6ecd4a088a45473b39aa7
bramadityaw <rbramadityaario@gmail.com>
2024-10-13 21:19:23 +0700

    feat: make the modal look nicer

--- resources/views/authors/destroy.blade.php
+++ resources/views/authors/destroy.blade.php
@@ -1,6 +1,6 @@
-<dialog class="border border-[#1B3C73] rounded-md p-4 w-4/5 md:w-1/6">
+<dialog class="border border-[#1B3C73] rounded-md p-4 w-2/5">
     <div class="text-center">
-        <p>Remove author?</p>
+        <p class="mb-4">Remove author?</p>
         <div class="flex flex-row justify-around text-white">
             <form method="dialog">
                 <button type="submit" class="rounded-md px-3 py-2 bg-blue-500">Batal</button>
